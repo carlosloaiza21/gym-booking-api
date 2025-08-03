bbae755d63dc7b97db2c9040e4bb4abfabd7cd86
Carlos Loaiza <cloaiza8602@gmail.com>
2025-08-03 06:37:29 -0600

    add await

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -30,7 +30,7 @@ export class AuthService {
             role: user.role
         }
         return {
-            access_token: this.jwtService.sign(payload)
+            access_token: await this.jwtService.sign(payload)
         }
     }
 }
